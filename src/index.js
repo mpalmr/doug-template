fc60282213f52024dbec2d890e1a92322eac7010
Matt Palmer <matt@silvercreekit.com>
2017-05-18 00:08:45 -0400

    Object mutation example across modules

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+'use strict';
 const sum = require('./sum');
+const objMutate = require('./objMutate');
+
+console.log(objMutate);
+objMutate.a = 'EYYO';
 
 console.log(sum(3, 9));
--- src/sum.js
+++ src/sum.js
@@ -1,1 +1,6 @@
+const objMutate = require('./objMutate');
+
+objMutate.a += 5;
+setTimeout(() => console.log(objMutate), 100);
+
 module.exports = (a, b) => a + b;
